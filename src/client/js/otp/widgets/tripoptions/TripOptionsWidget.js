42001af1f956838904d6db50cf28bc5d30b4a6b9
Klemen Kozelj <klemen.kozelj@gmail.com>
2015-09-15 14:40:25 +0200

    New Icons
    Dodane nove ikonice brez poravnave

--- src/client/js/otp/widgets/tripoptions/TripOptionsWidget.js
+++ src/client/js/otp/widgets/tripoptions/TripOptionsWidget.js
@@ -470,20 +470,20 @@ otp.widgets.tripoptions.ModeSelector =
         this.modeControls = [];
         this.optionLookup = {};
 
-        var transit     = '<input type="checkbox" class="TraversalMode" id="tmTransit" value="TRANSIT,WALK" checked>'+ _tr("Transit") +'<label for="tmTransit"></label>';
-        var bus         = '<input type="checkbox" class="TraversalMode" id="tmBus" value="BUSISH,WALK" disabled>'+ _tr("Bus Only") +'<label for="tmBus"></label>';
-        var train       = '<input type="checkbox" class="TraversalMode" id="tmTrain" value="TRAINISH,WALK" disabled> '+ _tr("Rail Only") +' <label for="tmTrain"></label><br>';
+        var transit     = '<input type="checkbox" class="TraversalMode" id="tmTransit" value="TRANSIT,WALK" checked><label for="tmTransit"></label>';
+        var bus         = '<input type="checkbox" class="TraversalMode" id="tmBus" value="BUSISH,WALK" disabled><label for="tmBus"></label>';
+        var train       = '<input type="checkbox" class="TraversalMode" id="tmTrain" value="TRAINISH,WALK" disabled><label for="tmTrain"></label><br>';
         
-        var walk        = '<input type="checkbox" class="TraversalMode" id="tmWalk" value="WALK" disabled>' + _tr('Walk Only') + '<label for="tmWalk"></label>';
-        var bicycle     = '<input type="checkbox" class="TraversalMode" id="tmBicycle" value="BICYCLE">' + _tr("Bicycle Only") + '<label for="tmBicycle"></label>';
-        var drive       = '<input type="checkbox" class="TraversalMode" id="tmCar" value="CAR" disabled>' + _tr("Drive Only") + '<label for="tmCar"></label><br>';
+        var walk        = '<input type="checkbox" class="TraversalMode" id="tmWalk" value="WALK" disabled><label for="tmWalk"></label>';
+        var bicycle     = '<input type="checkbox" class="TraversalMode" id="tmBicycle" value="BICYCLE"><label for="tmBicycle"></label>';
+        var drive       = '<input type="checkbox" class="TraversalMode" id="tmCar" value="CAR" disabled><label for="tmCar"></label><br>';
         
-        var parkAndRide = '<input type="checkbox" class="TraversalMode" id="tmParkRide" value="CAR_PARK,WALK,TRANSIT" disabled>' + _tr("Park and Ride") + '<label for="tmParkRide"></label>';
-        var kissAndRide = '<input type="checkbox" class="TraversalMode" id="tmKissRide" value="CAR,WALK,TRANSIT" disabled>'  + _tr("Kiss and Ride") + '<label for="tmKissRide"></label>';
-        var bikeAndRide = '<input type="checkbox" class="TraversalMode" id="tmBikeRide" value="BICYCLE_PARK,WALK,TRANSIT" disabled>' + _tr("Bike and Ride") + '<label for="tmBikeRide"></label><br>';
+        var parkAndRide = '<input type="checkbox" class="TraversalMode" id="tmParkRide" value="CAR_PARK,WALK,TRANSIT" disabled><label for="tmParkRide"></label>';
+        var kissAndRide = '<input type="checkbox" class="TraversalMode" id="tmKissRide" value="CAR,WALK,TRANSIT" disabled><label for="tmKissRide"></label>';
+        var bikeAndRide = '<input type="checkbox" class="TraversalMode" id="tmBikeRide" value="BICYCLE_PARK,WALK,TRANSIT" disabled><label for="tmBikeRide"></label><br>';
         
-        var carSharing  = '<input type="checkbox" class="TraversalMode" id="tmCarSharing" value="CARRENT">' + _tr("Rented Car") + '<label for="tmCarSharing"></label>';
-        var bikeSharing = '<input type="checkbox" class="TraversalMode" id="tmBikeSharing" value="WALK,BICYCLE_RENT"> '+ _tr("Rented Bicycle") + '<label for="tmBikeSharing"></label<br>';
+        var carSharing  = '<input type="checkbox" class="TraversalMode" id="tmCarSharing" value="CARRENT"><label for="tmCarSharing"></label>';
+        var bikeSharing = '<input type="checkbox" class="TraversalMode" id="tmBikeSharing" value="WALK,BICYCLE_RENT"><label for="tmBikeSharing"></label<br>';
         
         $(transit).appendTo(this.$());
         $(bus).appendTo(this.$());
